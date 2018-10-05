7a76717138afe627b371b0530195303f3bf29a1c
krzychna <kkrzys17@gmail.com>
2018-10-05 21:02:42 +0200

    Major rewritting of old code, added information page

--- scripts/psd1.js
+++
@@ -1,9 +0,0 @@
-var checkboxes = document.querySelectorAll('.square');
-var checks = checkboxes.childNodes;
-
-for (var i = 0; i < checkboxes.length; i++) {
-  checkboxes[i].addEventListener('click',function(e){
-    this.childNodes[1].classList.toggle('checked');
-    e.stopPropagation();
-    });
-}
